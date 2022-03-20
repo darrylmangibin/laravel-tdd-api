0418b41b46a165d8b4ece6db7148be5bdd8783af
Darryl Mangibin <darryl@goworkwize.com>
2022-03-20 13:01:59 +0800

    Fetch from database

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,20 +2,25 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_index_todo_list()
     {
-        $this->withoutExceptionHandling();
+        TodoList::create([
+            'name' => 'my list'
+        ]);
 
         $response = $this->getJson('api/todo-list');
 
