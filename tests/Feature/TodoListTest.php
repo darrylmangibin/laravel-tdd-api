669e1ccfbf596867bb30515c4831ac8c1f9b1f0d
Darryl Mangibin <darryl@goworkwize.com>
2022-03-20 11:47:29 +0800

    Add lodo-list route index

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->getJson('api/todo-list');
+
+        $this->assertEquals(1, count($response->json()));
+    }
+}
